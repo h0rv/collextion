75a0b049425129154c1f4efa66f3f7c4b0ed64b2
Robby <h0rv@users.noreply.github.com>
2023-03-27 22:20:41 -0400

    Update readme

--- src/model.py
+++ src/model.py
@@ -1,33 +1,28 @@
-from spacy.training import Example
-from spacy.tokens import DocBin
 import spacy
 import json
-from tqdm import tqdm
-import random
-import requests
+from book_utils import get_transcript_path
+from spacy.training import Example
+from spacy.tokens import DocBin
+
 
 def load_data(file):
     with open(file, "r", encoding="utf-8") as f:
         data = json.load(f)
     return data
 
+
 def save_data(file, data):
-    with open(file, "w",encoding="utf-8") as f:
+    with open(file, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4)
 
-def test_model(model, text):
+
+def test_model(nlp, text):
     doc = nlp(text)
     results = []
     for ent in doc.ents:
         results.append(ent.text)
     return results
 
-def get_transcript_path(transcript_name: str) -> str:
-    """
-    Get full path to transcript
-    """
-    path = consts.transcripts_path + "/" + transcript_name
-    return path
 
 def extract_title_list(transcript):
     nlp = spacy.load("en_core_web_lg")
@@ -36,10 +31,16 @@ def extract_title_list(transcript):
     for ent in doc.ents:
         if ent.label_ == "WORK_OF_ART":
             book_name = ent.text
-            book_title_list.append(book_name)       
+            book_title_list.append(book_name)
     return book_title_list
 
-#test
-transcript = "The following is a conversation with Rana L. Kliubi, a pioneer in the field of emotion recognition and human centric artificial intelligence. She is the founder of Effectiva, deputy CEO of SmartEye, author of Girl Decoded ,and one of the most brilliant, kind, inspiring, and fun human beings I've gotten the chance to talk to. This is the Lex Freidman podcast"
-book_title_list = extract_title_list(transcript)
-print(book_title_list)
+
+def test():
+    transcript = "The following is a conversation with Rana L. Kliubi, a pioneer in the field of emotion recognition and human centric artificial intelligence. She is the founder of Effectiva, deputy CEO of SmartEye, author of Girl Decoded ,and one of the most brilliant, kind, inspiring, and fun human beings I've gotten the chance to talk to. This is the Lex Freidman podcast"
+    book_title_list = extract_title_list(transcript)
+    print(book_title_list)
+
+
+if __name__ == "__main__":
+    test()
+
