af0636942dadf6b20426c26fd9d04ce822aa2688
frankieattea <79564167+frankieattea@users.noreply.github.com>
2023-03-29 13:21:43 -0400

    Update model ,gets more info from Google Books API

--- src/model.py
+++ src/model.py
@@ -1,5 +1,8 @@
+#model.py
 import json
 import spacy
+import requests
+
 
 from typing import List
 
@@ -9,19 +12,34 @@ MODEL_SIZE = "en_core_web_sm" # small
 
 
 model = None
-def extract_books(transcript: str) -> List[str]:
+def extract_books(transcript: str) -> List[dict]:
     global model
-    if model == None:  # avoid reloading model for each transcript
+    if model == None:
         model = spacy.load(MODEL_SIZE)
 
     doc = model(transcript)
     books = []
     for ent in doc.ents:
         if ent.label_ == "WORK_OF_ART":
-            book = ent.text
-            books.append(book)
-
+            book_title = ent.text
+            # Call Google Books API to get book details
+            url = f"https://www.googleapis.com/books/v1/volumes?q={book_title}"
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.json()
+                if "items" in data:
+                    # Get the first item in the list of books returned by the API
+                    book = data["items"][0]
+                    # Extract book details
+                    book_dict = {}
+                    book_dict['title'] = book['volumeInfo']['title']
+                    book_dict['authors'] = book['volumeInfo'].get('authors', ['']) #ran into this error, case where they don't have author
+                    book_dict['isbn'] = book['volumeInfo'].get('industryIdentifiers', [{'identifier': ''}])[0]['identifier']
+                    book_dict['image_url'] = book['volumeInfo'].get('imageLinks', {}).get('thumbnail')
+                    book_dict['link'] = book['volumeInfo'].get('canonicalVolumeLink', '')
+                    books.append(book_dict)
     return books
+
 
 
 def load_data(file):
